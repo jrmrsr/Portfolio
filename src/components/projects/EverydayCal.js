6d1232957b03006308edffc0622687c289d08332
jrmrsr <jjr_go@outlook.com>
2019-01-21 21:40:13 -0500

    Added in photo and small copy change.

--- src/components/projects/EverydayCal.js
+++ src/components/projects/EverydayCal.js
@@ -7,6 +7,7 @@ export default class EverydayCal extends Component {
     return (
       <div className="animated fadeInUpBig">
         <span><h2 className="project-title">The Everyday Calendar</h2><hr /></span>
+        <span className="img-container"><img src={everydayCalPic} alt="Everyday Calendar"/></span>
         <span><h2><a href="https://jrmrsr.github.io/Everyday-Calendar/" target="_blank" rel="noopener noreferrer">Link</a></h2></span>
         <p>> Inspired by <a href="https://www.kickstarter.com/projects/simonegiertz/the-every-day-calendar" target="_blank" rel="noopener noreferrer">Simone Giertz's kickstarter</a> and <a href="https://www.youtube.com/watch?v=-lpvy-xkSNA" target="_blank" rel="noopener noreferrer">video</a>.
         After listening to <a href="http://www.tristanharris.com/" target="_blank" rel="noopener noreferrer">Tristan Harris</a> speak about time well spent, 
@@ -14,7 +15,6 @@ export default class EverydayCal extends Component {
         their time is one of the most important problems in our society. As the attention economy is intent on taking
         more and more mind-share, we must take care of how we spend our time. The web application aims to help users 
         track habits that work towards mindfulness while providing a minimalist visual display that encourages consistency.</p>
-        <span className="img-container"><img src={everydayCalPic} alt="Everyday Calendar"/></span>
         <span className="project-button">{this.props.button}</span>
       </div>
     );
--- src/components/projects/SolarCar.js
+++ src/components/projects/SolarCar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import solarCarPic from '../../img/solarcar.jpg';
 import '../../App.css';
 
 export default class SolarCar extends Component {
@@ -6,12 +7,13 @@ export default class SolarCar extends Component {
     return (
       <div className="animated fadeInUpBig">
         <span><h2 className="project-title">Midnight Sun Solar Car</h2><hr /></span>
+        <span className="img-container"><img src={solarCarPic} alt="Solar Car in Action"/></span>
         <p>> High performance vehicles were my introduction into engineering and my motivation in
           joining a vehicle design team. I joined Midnight Sun last summer and spent a lot of that 
-          time helping build that year’s solar car and learning what it takes to build an efficient car. 
+          time helping build last year’s solar car and learning what it takes to build an efficient car. 
           This year, I am focusing on the structural composites that will make the chassis lighter and
-          stiffer. The aim is to create a competitive car that is improved from last year, and have a 
-          fair bit of fun along the way. </p>
+          stiffer, as well as vehicle dynamics. The aim is to create a competitive car that is improved from last year, and have a 
+          fair bit of fun along the way. <em>*The picture shows the car in action and was taken from the <a href="https://www.facebook.com/uwmidsun/" target="_blank" rel="noopener noreferrer">Midnight Sun Facebook Page</a>.</em></p>
         <span className="project-button">{this.props.button}</span>
       </div>
     );
